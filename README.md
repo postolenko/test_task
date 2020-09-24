133fc1f947455dd5b7577e6abf063981ed71d90c
postolenko <denis.postolenko@gmail.com>
2020-09-24 21:36:35 +0300

    Test

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/test_task/index.html
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+  $(".logos_slide").mCustomScrollbar({
+    axis:"x",
+    scrollButtons:{
+      enable: true,
+      scrollType: "continuous",
+      scrollSpeed: 20,
+      scrollAmount: 40
+    }
+  });
+});
